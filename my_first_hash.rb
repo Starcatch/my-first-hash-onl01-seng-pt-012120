95afd9c018a04c3c3ee119d1aa2ad14afb19351d
Timur Mishiev <timurmishiev@gmail.com>
2020-02-01 20:51:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,11 @@ def retrieval
 end
 
 
-def adding shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3
+def adding shipping_manifest = {"whale bone corsets" => 5,
+                       "porcelain vases" => 2,
+                       "oil paintings" => 3,
+                       "muskets" => 2,
+                       "gun powder" => 4
                  }
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
